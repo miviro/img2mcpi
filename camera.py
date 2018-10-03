cffa4b2347bbd2a1781e3f4ff50b48b6bc913b60
eonmilu <eonmilu@gmail.com>
2018-10-03 12:17:36 +0200

    Read all RGB values from the image

--- camera.py
+++ camera.py
@@ -1,4 +1,5 @@
 from picamera import PiCamera
+from PIL import Image
 from utils import *
 
 camera = PiCamera()
@@ -7,3 +8,9 @@ max_size = (128, 128)
 
 camera.capture(image_name)
 scaled_image = scaleImageTo(image_name, max_size)
+
+loaded_image = scaled_image.load()
+width, height = scaled_image.size
+for i in range(width):
+    for j in range(height):
+        print(loaded_image[i, j])
