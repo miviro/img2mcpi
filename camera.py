523ba56737001f97437041ae750aaea2b0fa23bc
eonmilu <eonmilu@gmail.com>
2018-10-05 11:51:47 +0200

    Check if config file exists

--- camera.py
+++ camera.py
@@ -1,16 +1,25 @@
 from picamera import PiCamera
-from PIL import Image
 from utils import *
 
 camera = PiCamera()
 image_name = "photo.png"
 max_size = (128, 128)
+config_path = "config.json" 
 
+if !configExists(config_path):
+    print("Config file not found. Creating...")
+    createConfig(config_path)
+print("Using config file.")
+
+print("Taking photo...")
 camera.capture(image_name)
+print("Rescaling image...")
 scaled_image = scaleImageTo(image_name, max_size)
 
 loaded_image = scaled_image.load()
 width, height = scaled_image.size
-for i in range(width):
-    for j in range(height):
-        print(loaded_image[i, j])
+print("Scaled photo dimensions: ", width, height)
+for x in range(width):
+    for y in range(height):
+        r, g, b, t = loaded_image[x, y]
+        findBestMatch(r, g, b, t)
--- utils.py
+++ utils.py
@@ -1,6 +1,16 @@
 from PIL import Image
+import os.path
 
 def scaleImageTo(name, max_size):
     raw_image = Image.open(name)
     raw_image.thumbnail(max_size, Image.ANTIALIAS)
     return raw_image
+
+def findBestMatch(r, g, b, t):
+    pass
+
+def configExists(path):
+    return os.path.isfile("path")
+
+def createConfig(path):
+    pass
