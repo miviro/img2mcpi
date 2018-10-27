c35ffda51e50fb9d891f990c6655d6d7a4eecf40
eonmilu <eonmilu@gmail.com>
2018-10-27 20:42:32 +0200

    Refactor

--- camera.py
+++ camera.py
@@ -8,28 +8,29 @@ from PIL import Image
 print("Connecting to Minecraft")
 mc = minecraft.Minecraft.create()
 pos = mc.player.getTilePos()
-pos = minecraft.Vec3(float(pos.x), float(pos.y), float(pos.z))
+pos = minecraft.Vec3(pos.x, pos.y, pos.z)
+
 max_size = (64, 64)
 config_path = "./config.json"
 scaled_image = None
 image_name = "photo.png"
 scaled_name = "sphoto.png"
 
-n_name = input("Select image path. Empty for camera image: ")
-if n_name == "":
+custom_path = input("Select image path. Empty for camera image: ")
+if custom_path == "":
     camera = PiCamera()
-    
+
     print("Taking photo...")
     camera.capture(image_name)
 else:
-    image_name = n_name
+    image_name = custom_path
 
 if not configExists(config_path):
     print("Config file not found. Creating...")
     createConfig(config_path)
+
 print("Using config file.")
 averages = readConfig(config_path)
-
 
 print("Rescaling image...")
 scaled_image = scaleImageTo(image_name, max_size)
@@ -40,18 +41,22 @@ width, height = scaled_image.size
 print("Scaled photo dimensions: ", width, height)
 print("Getting optimal blocks...")
 
-tpos = pos
+temp_pos = pos
 for y in range(height):
     for x in range(width):
-        tpos = minecraft.Vec3(tpos.x+1, tpos.y, tpos.z)
+        temp_pos = minecraft.Vec3(temp_pos.x+1, temp_pos.y, temp_pos.z)
         r, g, b, t = loaded_image[x, y]
-        t_best_match = findBestMatch(r, g, b, averages)
-        best_match = t_best_match[0]
-        
-        if len(t_best_match) == 1:
-            mc.setBlock(tpos, float(best_match))
+
+        full_best_match = findBestMatch(r, g, b, averages)
+        best_match_id = full_best_match[0]
+
+        # If there is only one item in full_best_match, assume the item is a simple ID
+        if len(full_best_match) == 1:
+            mc.setBlock(temp_pos, float(best_match_id))
+        # Else add the sub-id as an argument
         else:
-            mc.setBlock(tpos, float(best_match), float(t_best_match[1]))
-        
-    tpos = minecraft.Vec3(pos.x, tpos.y+1, tpos.z)
+            mc.setBlock(temp_pos, float(best_match_id), float(full_best_match[1]))
+
+    temp_pos = minecraft.Vec3(pos.x, temp_pos.y+1, temp_pos.z)
+    # Slow down to prevent connection losses
     time.sleep(1)
--- utils.py
+++ utils.py
@@ -13,20 +13,20 @@ def findBestMatch(r, g, b, averages):
     best_match_id = "undefined"
     best_diff = 255 + 255 + 255
     curr_diff = 0
-    rdiff, gdiff, bdiff = 0, 0, 0
-    
+    r_diff, g_diff, b_diff = 0, 0, 0
+
     for i in averages:
-        rdiff = abs(i["rgb"][0] - r)
-        gdiff = abs(i["rgb"][1] - g)
-        bdiff = abs(i["rgb"][2] - b)
-        curr_diff = rdiff + gdiff + bdiff
-        
+        r_diff = abs(i["rgb"][0] - r)
+        g_diff = abs(i["rgb"][1] - g)
+        b_diff = abs(i["rgb"][2] - b)
+        curr_diff = r_diff + g_diff + b_diff
+
         if best_diff > curr_diff:
             best_diff = curr_diff
             best_match_id = i["id"]
-    
+
     print(best_match_id.split(":"))
-    
+
     return best_match_id.split(":")
 
 def configExists(path):
@@ -38,46 +38,45 @@ def createConfig(path):
         for line in materials:
             image_name, true_id = getLineProps(line)
             image = Image.open("./blocks/"+image_name)
-            pic = image.load()
-            
+            loaded_image = image.load()
+
             width, height = image.size
-            r, g, b = getAverageColor(width, height, pic)
-            
+            r, g, b = getAverageColor(width, height, loaded_image)
+
             block_info = {"name": image_name, "id": true_id, "rgb": (int(r), int(g), int(b))}
             cfg.append(block_info)
-            
+
     with open(path, "w") as file:
         file.write(json.dumps(cfg))
 
 def readConfig(path):
     with open(path, "r") as conf:
         return json.loads(conf.read())
-        
+
 def getLineProps(line):
     line_values = line.split(",")
-    image_name = re.findall("(?<=,)[^,]*\.png", line)[0]
-    
+    image_name = re.findall(r"(?<=,)[^,]*\.png", line)[0]
+
     true_id = ""
     primary_id = line_values[3]
     secondary_id = line_values[4]
+
     if secondary_id == "-1":
         true_id = primary_id
     else:
         true_id = primary_id+":"+secondary_id
-    
-    return image_name, true_id
 
+    return image_name, true_id
 
 def getAverageColor(x, y, image):
     r, g, b = 0, 0, 0
     count = 0
     for s in range(x):
         for t in range(y):
-            pixlr, pixlg, pixlb, pixlt = image[s, t]
-            r += pixlr
-            g += pixlg
-            b += pixlb
+            rgb = image[s, t]
+            r += rgb[0] # Red
+            g += rgb[1] # Blue
+            b += rgb[2] # Green
+
             count += 1
     return ((r/count), (g/count), (b/count))
-
-
